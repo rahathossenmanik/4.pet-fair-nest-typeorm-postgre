1bf322bd3ffffbea2441e89915ff885c7eda2777
Manik Hosen <manik.hosen@bizzntek.com>
2023-04-09 16:27:39 +0600

    ci: controller

--- src/controllers/authors.controller.ts
+++ src/controllers/authors.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { Author } from 'src/schemas/authors.schema';
+import { Author } from 'src/entities/authors.entity';
 import { AuthorService } from 'src/services/authors.service';
 
 @ApiTags('Authors')
@@ -14,22 +14,22 @@ export class AuthorsController {
   }
 
   @Get(':id')
-  findById(@Param('id') id: string) {
+  findById(@Param('id') id: number) {
     return this.authorsService.findById(id);
   }
 
-  @Post()
-  create(@Body() author: Author) {
-    return this.authorsService.create(author);
-  }
+  // @Post()
+  // create(@Body() author: Author) {
+  //   return this.authorsService.create(author);
+  // }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() author: Author) {
-    return this.authorsService.update(id, author);
-  }
+  // @Put(':id')
+  // update(@Param('id') id: string, @Body() author: Author) {
+  //   return this.authorsService.update(id, author);
+  // }
 
   @Delete(':id')
-  delete(@Param('id') id: string) {
+  delete(@Param('id') id: number) {
     return this.authorsService.delete(id);
   }
 }
--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { Book } from 'src/schemas/books.schema';
+import { Book } from 'src/entities/books.entity';
 import { BookService } from 'src/services/books.service';
 
 @ApiTags('Books')
@@ -14,22 +14,22 @@ export class BooksController {
   }
 
   @Get(':id')
-  findById(@Param('id') id: string) {
+  findById(@Param('id') id: number) {
     return this.booksService.findById(id);
   }
 
-  @Post()
-  create(@Body() book: Book) {
-    return this.booksService.create(book);
-  }
+  // @Post()
+  // create(@Body() book: Book) {
+  //   return this.booksService.create(book);
+  // }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() book: Book) {
-    return this.booksService.update(id, book);
-  }
+  // @Put(':id')
+  // update(@Param('id') id: string, @Body() book: Book) {
+  //   return this.booksService.update(id, book);
+  // }
 
   @Delete(':id')
-  delete(@Param('id') id: string) {
+  delete(@Param('id') id: number) {
     return this.booksService.delete(id);
   }
 }

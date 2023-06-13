087e48f3e7a31abe5eb24e0bfe2b56434903edb3
Manik Hosen <rahathossenmanik@gmail.com>
2023-06-13 23:43:10 +0600

    ci: dev url

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,4 +1,9 @@
-require('dotenv').config({ path: '.env' });
+require('dotenv').config({
+  path: '.env',
+});
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 import { Module } from '@nestjs/common';
 import { AuthorsController } from 'src/controllers/authors.controller';
 import { AuthorService } from 'src/services/authors.service';
@@ -9,18 +14,29 @@ import { Author } from 'src/entities/authors.entity';
 import { Book } from 'src/entities/books.entity';
 import { DataSource } from 'typeorm';
 
+const url =
+  'postgres://' +
+  process.env.DB_USER +
+  ':' +
+  process.env.DB_PASS +
+  '@' +
+  process.env.DB_HOST +
+  '/' +
+  process.env.DB_NAME;
+console.log(url);
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: 'postgres://postgres:admin@localhost/mern-mvc-crud',
       entities: [Author, Book],
-      synchronize: true
+      synchronize: true,
     }),
-    TypeOrmModule.forFeature([Author, Book])
+    TypeOrmModule.forFeature([Author, Book]),
   ],
   controllers: [AuthorsController, BooksController],
-  providers: [AuthorService, BookService]
+  providers: [AuthorService, BookService],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}

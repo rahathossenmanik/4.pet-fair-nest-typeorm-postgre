1be3730f2c4fab9fa2f815acc2476f6fd424fcf2
Manik Hosen <rahathossenmanik@gmail.com>
2023-09-03 20:03:08 +0600

    cors and swagger customized

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './module/app.module';
 
+const corsOption = {
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'https://zzkvcp-3000.csb.app',
+    'https://*.csb.app'
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
+
 async function bookstore() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Nest Bookshop')
-    .setDescription('Nest Bookshop CRUD API')
-    .setVersion('1.0')
+    .setTitle('Pet Fair')
+    .setDescription('Node JS + Express + Mongoose + MongoDB')
+    .setVersion('1.0.0')
+    .setContact('Email', 'mailto:rahathossenmanik@gmail.com', 'rahathossenmanik@gmail.com')
+    .setLicense('MIT', 'https://opensource.org/licenses/MIT')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.enableCors(corsOption);
 
-  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bookstore();
